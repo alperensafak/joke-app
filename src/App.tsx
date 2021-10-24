0102d2ad72da092c9215ad747d871d97731d6680
Alperen <alperensafak@gmail.com>
2021-10-25 01:01:09 +0300

    created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {Wrapper,Row,Header,Image,Form,Search} from "./components/styled/index"
+import Lottie from 'react-lottie';
+import animationData from './lotties/laugh.json';
 
 const App = () => {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+
     return (
         <div>
-          <h1>JOKE APP</h1>
+            <Wrapper>
+                <Row>
+                    <Header>JOKE APP</Header>
+                    <Image> <Lottie options={defaultOptions} /></Image>
+                </Row>
+
+            </Wrapper>
         </div>
     );
 };
